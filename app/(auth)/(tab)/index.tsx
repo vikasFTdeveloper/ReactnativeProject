2571b6198ada013bb62bee3b8492e0695db6d02b
Vikas <vikas721a@gmail.com>
2026-07-15 18:35:18 +0530

    fonts implements

--- app/(auth)/(tab)/index.tsx
+++ app/(auth)/(tab)/index.tsx
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <SafeAreaView className= "flex-1 bg-background p-5">
 
-      <Text className="text-xl font-sans-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
+
+
       <Link href="/Onboarding" className="mt-4 rounded bg-primary text-white p-4">go to Onboarding</Link>
       <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">go to sign-in</Link>
       <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">go to sign-up</Link>
